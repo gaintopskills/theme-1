4325c076ee1638e34a4575c0967f153a73c17254
gaintopskills <raynafit@yahoo.com>
2024-10-20 17:30:12 -0700

    1

---
+++ src/components/Slider.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Slider = () => {
+  return (
+    <div>
+      <h1 style={{ color: '#ffffff' }}>Sample Elements</h1>
+      <div className="animated-text">
+        <div className="sp-container">
+          <div className="sp-content">
+            <div className="sp-globe"></div>
+            <h2 className="frame-1">E&R Unlimited Inc.</h2>
+            <h2 className="frame-2">Embroidery</h2>
+            <h2 className="frame-3">Patches</h2>
+            <h2 className="frame-4">Sublimation</h2>
+            <h2 className="frame-5">
+              <span>Where quality</span>
+              <span> and </span>
+              <span>innovation matter.</span>
+            </h2>
+            <a className="sp-circle-link" href="https://nick.mkrtchyan.ga">
+              by Nick
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
