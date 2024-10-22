ce8643045b82cdbb3a5e68a3a744ad6b14345a0f
gaintopskills <raynafit@yahoo.com>
2024-10-21 18:45:45 -0700

    1

--- src/components/StitchingAnimation.jsx
+++ src/components/StitchingAnimation.jsx
@@ -4,16 +4,9 @@ export const StitchingAnimation = () => {
   return (
     <div className="responsive-banner-container">
       <svg viewBox="0 0 1200 300" className="responsive-banner-svg">
-        {/* Main company name with handwriting effect */}
-        <text className="banner-text" x="50%" y="50%" dominantBaseline="middle" textAnchor="middle">
+        <text className="banner-text" x="50%" y="50%">
           E & R Unlimited Inc.
         </text>
-        {/* Fading in second line */}
-        <text className="fade-in-text" x="50%" y="70%" dominantBaseline="middle" textAnchor="middle">
-          Where quality and innovation matter.
-        </text>
-        {/* Optional border around the banner */}
-        <rect className="lux-border" x="10" y="10" width="1180" height="280" rx="20" ry="20" />
       </svg>
     </div>
   );
