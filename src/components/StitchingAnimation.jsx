9a8f1d6ee4db9fa620b84601b14dd953a9d55498
gaintopskills <raynafit@yahoo.com>
2024-10-21 19:07:29 -0700

    1

--- src/components/StitchingAnimation.jsx
+++ src/components/StitchingAnimation.jsx
@@ -4,23 +4,22 @@ export const StitchingAnimation = () => {
   const [showSubtitle, setShowSubtitle] = useState(false);
 
   useEffect(() => {
-    // Trigger the subtitle to fade in after a shorter time
     const timer = setTimeout(() => {
       setShowSubtitle(true);
-    }, 500); // Reduced the delay to 2 seconds
+    }, 500); 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
-      <svg viewBox="0 0 1200 300" className="w-full h-auto">
+      <svg viewBox="0 0 1200 300" className="w-full h-auto px-8">
         <text className="banner-text font-dancing text-[10vw] text-white" x="50%" y="50%" dominantBaseline="middle" textAnchor="middle">
           E & R Unlimited Inc.
         </text>
       </svg>
 
       {showSubtitle && (
-        <div className="mt-4 bg-white text-black p-4 text-xl transition-opacity duration-5000 opacity-100 ease-in">
+        <div className="fade-in mt-4 text-black bg-white p-4 text-3xl transition-opacity opacity-100">
           Where quality and innovation matter.
         </div>
       )}
