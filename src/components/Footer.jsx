24778e5dbada9511c31db738d4733ee3b2a59c94
gaintopskills <raynafit@yahoo.com>
2024-10-22 03:36:49 -0700

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const footerData = [
 export const Footer = () => {
   return (
     <footer aria-label="Site footer">
-      <div className="pt-10  lg:pt-20 lg:pb-16 bg-black radius-for-skewed ">
+      <div className="pt-10 lg:pt-20 lg:pb-16 bg-black radius-for-skewed">
         <div className="container mx-auto px-4 w-4/5 md:w-11/12 lg:w-10/12 xl:w-4/5 2xl:w-2/3">
           <div className="flex flex-wrap">
             <div className="w-full lg:w-1/3 mb-16 lg:mb-0">
@@ -46,7 +46,7 @@ export const Footer = () => {
                 <a
                   href="https://bootstrapious.com/p/bootstrap-4-dark-admin"
                   target="_blank"
-                  className="text-gray-100 ml-1.5 "
+                  className="text-gray-100 ml-1.5"
                   aria-label="Dark Admin Dashboard"
                 >
                   here.
@@ -54,21 +54,21 @@ export const Footer = () => {
               </p>
               <div className="w-36 mx-auto lg:mx-0">
                 <a
-                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
+                  className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
                   href="#"
                   aria-label="Facebook"
                 >
                   <FacebookIcon />
                 </a>
                 <a
-                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
+                  className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
                   href="#"
                   aria-label="Twitter"
                 >
                   <TwitterIcon />
                 </a>
                 <a
-                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
+                  className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
                   href="#"
                   aria-label="Instagram"
                 >
@@ -76,9 +76,11 @@ export const Footer = () => {
                 </a>
               </div>
             </div>
-            <div className="w-full lg:w-2/3  lg:pl-16 hidden lg:flex flex-wrap justify-between">
+            <div className="w-full lg:w-2/3 lg:pl-16 hidden lg:flex flex-wrap justify-between">
               <div className="w-full md:w-1/3 lg:w-auto mb-16 md:mb-0">
-                <h3 className="mb-6 text-2xl font-bold text-primaryText">Products</h3>
+                <h3 className="mb-6 text-2xl font-bold text-primaryText">
+                  Products
+                </h3>
                 <ul>
                   {footerData[0].items.map((item, index) => (
                     <li key={`${item}-${index}`} className="mb-4">
@@ -112,7 +114,9 @@ export const Footer = () => {
                 </ul>
               </div>
               <div className="w-full md:w-1/3 lg:w-auto">
-                <h3 className="mb-6 text-2xl font-bold text-primaryText">Company</h3>
+                <h3 className="mb-6 text-2xl font-bold text-primaryText">
+                  Company
+                </h3>
                 <ul>
                   {footerData[2].items.map((item, index) => (
                     <li key={`${item}-${index}`} className="mb-4">
@@ -130,6 +134,11 @@ export const Footer = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Oversized "E & R Unlimited" Text */}
+      <div className="bg-black text-white text-center p-4" style={{ marginBottom: 0, paddingBottom: 0 }}>
+        <div className="text-[10vw] font-bold leading-none">E & R Unlimited</div>
       </div>
     </footer>
   );
