188317e8d6413893e2aa65cdbd6e0eef731824b8
gaintopskills <raynafit@yahoo.com>
2024-11-23 11:22:03 -0800

    1

--- src/components/Features2.jsx
+++ src/components/Features2.jsx
@@ -24,7 +24,7 @@ export const Features2 = () => (
 
 <p>We create precise, vibrant designs with high-quality finishes using advanced stitching techniques and premium threads. Whether you need a professional logo, intricate design, or personalized text, our embroidery services deliver lasting results on a variety of fabrics.</p>
 
-<p>Elevate your brand with our exceptional embroidery solutions - designed for style and durability.</p>
+<p>Elevate your brand with our exceptional embroidery solutions - designed for style and durability.
             </p>
             <h2 className="mt-6 mb-8 text-4xl lg:text-5xl block-big-title">
               Screen Printing
