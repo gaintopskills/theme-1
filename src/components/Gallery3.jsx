e00d88ca0aef81580772518904d7d69f6d4f89a8
gaintopskills <raynafit@yahoo.com>
2024-12-26 11:32:26 -0800

    1

--- src/components/Gallery3.jsx
+++ src/components/Gallery3.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 const images = [
-  "https://images.unsplash.com/photo-1566679056462-2075774c8c07?q=80&w=2675&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=2667&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1670414701148-16ac8873a150?q=80&w=2648&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=2856&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1526772662000-3f88f10405ff?q=80&w=2748&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1443632864897-14973fa006cf?q=80&w=2670&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1483921020237-2ff51e8e4b22?q=80&w=2670&auto=format&fit=crop",
+  "../product1.jpg",
+  "../product2.jpg",
+  "../product3.jpg",
+  "../product4.jpg",
+  "../product5.jpg",
+  "../product6.jpg",
+  "../product7.jpg",
+  "../product8.jpg",
 ];
 
 export const Gallery3 = () => {
