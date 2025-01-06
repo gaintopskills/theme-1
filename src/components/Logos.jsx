4c6c92b62b7d54b656c10b70530cd881fcad01f2
null <null>
2025-01-06 12:05:16 -0800

    1

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const logos = [
   { src: '/30-seconds-to-mars.webp', alt: '30 seconds to mars', width: 130, height: 50  },
-  { src: '/frame-denim.webp', alt: 'Logo  Frame Demin', width: 447, height: 50  },
+  { src: '/frame-denim.webp', alt: 'Frame Demin logo', width: 447, height: 50  },
   { src: '/joes.webp', alt: 'Joes', width: 189, height: 50  },
   { src: '/levis.webp', alt: 'Logo Levis logo', width: 226, height: 50  },
   { src: '/NTWRK.webp', alt: 'NTWRK', width: 299, height: 50  },
