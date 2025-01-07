93aa3085761e0c603d4c8308d6071a2494649c27
null <null>
2025-01-06 20:12:04 -0800

    1

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -4,7 +4,7 @@ const logos = [
   { src: '/30-seconds-to-mars.webp', alt: '30 seconds to mars', width: 78, height: 30  },
   { src: '/frame-denim.webp', alt: 'Frame Demin logo', width: 263, height: 30  },
   { src: '/joes.webp', alt: 'Joes', width: 130, height: 30  },
-  { src: '/levis.webp', alt: 'Logo Levis logo', width: 226, height: 30  },
+  { src: '/levis.webp', alt: 'Logo Levis logo', width: 136, height: 30  },
   { src: '/NTWRK.webp', alt: 'NTWRK', width: 299, height: 30  },
   { src: '/petite-plume.webp', alt: 'NTWRK', width: 421, height: 30  },
   { src: '/todd-snyder.webp', alt: 'NTWRK', width: 224, height: 30  },
