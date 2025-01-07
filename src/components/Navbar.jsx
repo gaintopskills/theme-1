de07d830426f1d1dd0a6cf1bf44c4ec7f4464cca
null <null>
2025-01-07 15:04:15 -0800

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const navbarLinks = [
     children: [
       { label: 'Embroidery', href: '/sub-feature3', ariaLabel: 'Sub-feature 3', image: '/menu3.jpg' },
       { label: 'Screen Printing', href: '/sub-feature4', ariaLabel: 'Sub-feature 4', image: '/menu5.jpg' },
-      { label: 'DTG', href: '/sub-feature5', ariaLabel: 'Sub-feature 5', image: '/menu1.jpg' },
+      { label: 'DTG', href: '/sub-feature5', ariaLabel: 'Sub-feature 5', image: '/dtg-services.webp' },
       { label: 'Digital Sublimation', href: '/sub-feature6', ariaLabel: 'Sub-feature 6', image: '/menu4.jpg' },
       { label: 'Heat Transfer', href: '/sub-feature7', ariaLabel: 'Sub-feature 7', image: '/menu2.jpg' },
       { label: 'Cut & Saw', href: '/sub-feature6', ariaLabel: 'Sub-feature 6', image: '/menu6.jpg' },
