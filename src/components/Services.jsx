baa328fbcac8d88eabed8ccef069ddd1920ecc67
gaintopskills <raynafit@yahoo.com>
2025-01-08 14:55:32 -0800

    1

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -62,18 +62,20 @@ export const ServicesSection = () => {
             whileInView={{ opacity: 1 }}
             viewport={{ once: true, amount: 0.2 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
-            className="flex flex-col md:flex-row items-center md:items-start space-x-0 md:space-x-6 p-4 rounded-md transition duration-300 hover:bg-gray-800 hover:shadow-lg hover:scale-105 transform"
+            className="flex md:flex-row items-start md:items-center p-4 rounded-md transition duration-300 hover:bg-gray-800 hover:shadow-lg hover:scale-105 transform"
           >
-            <figure className="flex-shrink-0 overflow-hidden rounded-md mb-4 md:mb-0">
+            {/* Image Wrapper */}
+            <div className="flex items-center justify-center w-24 h-24 flex-shrink-0">
               <a href={service.link}>
                 <img
                   src={service.image}
                   alt={service.title}
-                  className="w-24 h-24 object-cover transition duration-300 transform hover:scale-105"
+                  className="w-full h-full object-cover transition duration-300 transform hover:scale-105 rounded-md"
                 />
               </a>
-            </figure>
-            <div className="text-center md:text-left">
+            </div>
+            {/* Text Content */}
+            <div className="flex flex-col text-center md:text-left ml-4">
               <h3
                 className="text-2xl sm:text-3xl font-bold text-white hover:text-gray-400 transition-colors"
                 style={{ fontFamily: "'Dancing Script', cursive" }}
@@ -81,12 +83,20 @@ export const ServicesSection = () => {
                 <a href={service.link}>{service.title}</a>
               </h3>
               <p className="text-gray-400">{service.description}</p>
-              <a
-                href={service.link}
-                className="mt-4 inline-block px-4 py-2 border-gray-700 bg-gray-700 text-white rounded-md hover:bg-gray-500 transition duration-300"
-              >
-                Learn More
-              </a>
+              <div className="flex mt-4 space-x-4">
+                <a
+                  href={service.link}
+                  className="px-4 py-2 bg-bgDark1 text-white rounded-md hover:bg-gray-500 transition duration-300 inline-block"
+                >
+                  Learn More
+                </a>
+                <a
+                  href="#quote"
+                  className="px-4 py-2 bg-red-800 text-white rounded-md hover:bg-red-600 transition duration-300 inline-block"
+                >
+                  Get a Quote
+                </a>
+              </div>
             </div>
           </motion.div>
         ))}
